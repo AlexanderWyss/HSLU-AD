24b4b0db7e6d197254e05996bfce3baa998239b6
Alexander Wyss <alexsilvan.wyss@gmail.com>
2021-10-11 14:36:50 +0200

    übungen d2 extract interface

--- src/main/java/d2/Tree.java
+++ src/main/java/d2/Tree.java
@@ -1,125 +1,28 @@
 package d2;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
 import java.util.function.BiConsumer;
 
-/**
- * Binary Tree Map, allows null values
- *
- * @param <K> Type of key, must be comparable
- * @param <V> Type of value
- */
-public class Tree<K extends Comparable<K>, V> {
-    private Node<K, V> root;
+public interface Tree<K extends Comparable<K>, V> {
+    /**
+     * Add or set value for corresponding key.
+     *
+     * @param key   not null
+     * @param value value to be associated with key
+     */
+    void add(K key, V value);
 
-    public void add(K key, V value) {
-        if (root == null) {
-            root = new Node<>(key, value);
-        } else {
-            Node<K, V> closestNode = findClosestNode(root, key);
-            setValueForComparisonResult(closestNode, key, value);
-        }
-    }
+    /**
+     * Get Value for corresponding key.
+     *
+     * @param key not null
+     * @return value associated with key or null if not exists
+     */
+    V get(K key);
 
-    private Node<K, V> findClosestNode(Node<K, V> node, K key) {
-        var childNode = getChildForComparisonResult(node, key);
-        if (childNode != null) {
-            return findClosestNode(childNode, key);
-        }
-        return node;
-    }
-
-    private Node<K, V> getChildForComparisonResult(Node<K, V> node, K key) {
-        int comparisonResult = node.compareTo(key);
-        if (comparisonResult > 0) {
-            return node.getLeft();
-        }
-        if (comparisonResult < 0) {
-            return node.getRight();
-        }
-        return null;
-    }
-
-    private void setValueForComparisonResult(Node<K, V> node, K key, V value) {
-        int comparisonResult = node.compareTo(key);
-        if (comparisonResult > 0) {
-            node.setLeft(new Node<>(key, value));
-        } else if (comparisonResult < 0) {
-            node.setRight(new Node<>(key, value));
-        } else {
-            node.setValue(value);
-        }
-    }
-
-    public V get(K key) {
-        Objects.requireNonNull(key);
-        if (root == null) {
-            return null;
-        }
-        var closestNode = findClosestNode(root, key);
-        if (closestNode.compareTo(key) == 0) {
-            return closestNode.getValue();
-        }
-        return null;
-    }
-
-    public void traverseInorder(BiConsumer<K, V> consumer) {
-        traverseInorder(root, consumer);
-    }
-
-    private void traverseInorder(Node<K, V> node, BiConsumer<K, V> consumer) {
-        if (node != null) {
-            traverseInorder(node.getLeft(), consumer);
-            consumer.accept(node.getKey(), node.getValue());
-            traverseInorder(node.getRight(), consumer);
-        }
-    }
-
-    public Node<K, V> getRoot() {
-        return root;
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        int nrOfNodesInLastLine = (int) Math.pow(2, height() - 1);
-        appendString(sb, List.of(root), nrOfNodesInLastLine);
-        return sb.toString();
-    }
-
-    private void appendString(StringBuilder sb, List<Node<K, V>> nodes, int nrOfNodesInLastLine) {
-        List<Node<K, V>> children = new ArrayList<>();
-        int paddingBefore = (nrOfNodesInLastLine / nodes.size()) - 1;
-        int paddingBetween = paddingBefore * 2 + 1;
-        sb.append(" ".repeat(paddingBefore));
-        for (var node : nodes) {
-            if (node != null) {
-                sb.append(node.getKey());
-                children.add(node.getLeft());
-                children.add(node.getRight());
-            } else {
-                sb.append("-");
-                children.add(null);
-                children.add(null);
-            }
-            sb.append(" ".repeat(paddingBetween));
-        }
-        sb.append(System.lineSeparator());
-        if (children.stream().anyMatch(Objects::nonNull)) {
-            appendString(sb, children, nrOfNodesInLastLine);
-        }
-    }
-
-    public int height() {
-        return height(root);
-    }
-
-    private int height(Node<K, V> node) {
-        if (node == null) {
-            return 0;
-        }
-        return 1 + Math.max(height(node.getLeft()), height(node.getRight()));
-    }
+    /**
+     * Traverses tree in order and calls the consumer for every node
+     *
+     * @param consumer BiConsumer with params key, value
+     */
+    void traverseInorder(BiConsumer<K, V> consumer);
 }
--- src/test/java/d2/BinaryTreeTest.java
+++ src/test/java/d2/BinaryTreeTest.java
@@ -9,7 +9,7 @@ import java.util.function.BiConsumer;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-class TreeTest {
+class BinaryTreeTest {
     @Test
     void test() {
         Tree<String, String> tree = buildTree();
@@ -18,7 +18,7 @@ class TreeTest {
 
     @Test
     void multipleValues_add_correctTree() {
-        Tree<String, String> tree = buildTree();
+        BinaryTree<String, String> tree = buildTree();
 
         Node<String, String> nodeG = tree.getRoot();
         assertEquals("G", nodeG.getKey());
@@ -46,7 +46,7 @@ class TreeTest {
 
     @Test
     void emptyTree_addKeyNull_throwNullPointerException() {
-        Tree<String, String> tree = new Tree<>();
+        Tree<String, String> tree = new BinaryTree<>();
 
         assertThrows(NullPointerException.class, () -> tree.add(null, ""));
     }
@@ -87,14 +87,14 @@ class TreeTest {
 
     @Test
     void emptyTree_getWithNonExistentKey_returnsNull() {
-        Tree<String, String> tree = new Tree<>();
+        Tree<String, String> tree = new BinaryTree<>();
 
         assertNull(tree.get("nonExistentKey"));
     }
 
     @Test
     void emptyTree_getKeyNull_throwNullPointerException() {
-        Tree<String, String> tree = new Tree<>();
+        Tree<String, String> tree = new BinaryTree<>();
 
         assertThrows(NullPointerException.class, () -> tree.get(null));
     }
@@ -128,7 +128,7 @@ class TreeTest {
     @Test
     @SuppressWarnings("unchecked")
     void emptyTree_traversInOrder_traversedInOrder() {
-        Tree<String, String> tree = new Tree<>();
+        Tree<String, String> tree = new BinaryTree<>();
         BiConsumer<String, String> biConsumer = mock(BiConsumer.class);
 
         tree.traverseInorder(biConsumer);
@@ -137,8 +137,8 @@ class TreeTest {
     }
 
     @NotNull
-    private Tree<String, String> buildTree() {
-        Tree<String, String> tree = new Tree<>();
+    private BinaryTree<String, String> buildTree() {
+        BinaryTree<String, String> tree = new BinaryTree<>();
         String[] elements = "G H B F E A D C".split(" ");
         for (String element : elements) {
             tree.add(element, element + "_Value");
